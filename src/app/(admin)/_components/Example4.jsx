06f2742a6a07e0e5585b8947252ba4cb70c38c9c
Suyeon Sun <suninisy@gmail.com>
2025-06-02 09:24:33 +0900

    example4 iframe 생성

--- src/app/(admin)/_components/Example4.jsx
+++ src/app/(admin)/_components/Example4.jsx
@@ -5,7 +5,12 @@ import cytoscape from "@/lib/cytoscapeWithExtensions";
 export default function Example4() {
   return (
     <div className="overflow-hidden  border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
-      Example4
+      <iframe
+        src="/html/example4/index.html"
+        width="100%"
+        height="600"
+        frameBorder="0"
+      />
     </div>
   );
 }
